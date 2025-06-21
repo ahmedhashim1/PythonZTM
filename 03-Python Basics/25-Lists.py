9328b0416987a8f745eebff7a515743be091b7dc
Ahmed Hashim <ahmedhashim_2000@hotmail.com>
2025-06-21 21:25:35 +0500

    2nd Commit

--- 03-Python Basics/25-Lists.py
+++ 03-Python Basics/25-Lists.py
@@ -1,5 +1,8 @@
-li = [1,2,3,4,5]
-li2 = ['a','b','c','d']
-li3 = [1,2,'a','b',True]
+# li = [1,2,3,4,5]
+# li2 = ['a','b','c','d']
+# li3 = [1,2,'a','b',True]
 
 #Data Structure
+
+amazon_cart = ['notebooks', 'sunglasses']
+print(amazon_cart[1])
